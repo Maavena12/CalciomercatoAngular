b7389847964b64cb11f5ded240c247b33b8767c2
Mariosor12 <marioavena120898@gmail.com>
2024-10-10 16:09:52 -0400

    commit

--- calciomercato/src/app/header/header.component.ts
+++ calciomercato/src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -5,14 +6,19 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   @Input() searchTerm: string = ''; 
-  @Output() searchTermChange: EventEmitter<string> = new EventEmitter<string>();
+  @Output() searchTermChange: EventEmitter<string> = new EventEmitter<string>(); 
+  isMobileMenuOpen: boolean = false; 
   constructor(private router: Router) {}
+
+  toggleMobileMenu() {  
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;  
+  } 
 
   onSearchTermChange(): void {
     this.searchTermChange.emit(this.searchTerm);
