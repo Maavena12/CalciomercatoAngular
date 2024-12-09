d1220d58038edd46053a78ed953159e891422db0
Mariosor12 <marioavena120898@gmail.com>
2024-12-09 14:49:01 -0400

    commit

--- calciomercato/src/environments/environment.prod.ts
+++ calciomercato/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://calciomercatoangular-production.up.railway.app/api',
+    apiUrl: 'https://calciomercatoangular.onrender.com/api',
 };
--- calciomercato/src/environments/environment.ts
+++ calciomercato/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://calciomercatoangular-production.up.railway.app/api',
+    apiUrl: 'https://calciomercatoangular.onrender.com/api',
 };
